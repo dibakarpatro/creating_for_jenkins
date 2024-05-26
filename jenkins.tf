5b58a04ceab45d75ea5d67fc7da1c04ef23f0182
dibakarpatro <dibakarpatro60@gmail.com>
2024-05-26 14:51:01 +0530

    tfhjg

---
+++ jenkins.tf
@@ -0,0 +1,14 @@
+node {
+    stage('clone'){
+        git branch : 'main', url: 'https://github.com/dibakarpatro/creating_for_jenkins.git'
+    }
+    stage('init'){
+        echo 'terraform init'
+    }
+    stage('plan'){
+        echo 'terraform plan'
+    }
+    stage('apply'){
+        echo 'terraform apply -auto-approve'
+    }
+}
